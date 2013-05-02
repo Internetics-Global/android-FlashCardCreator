a3ff4a10ee0fd58af138e031150a8bad3cc3b671
Liang Wang <wangbourne@gmail.com>
2013-05-02 22:25:12 +1000

    Summary: 1. UI improvment; 2. add help view; 3. fix some bugs

---
+++ src/com/internectics/fragment/HelpFragment.java
@@ -0,0 +1,11 @@
+package com.internectics.fragment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BourneWang
+ * Date: 2/05/13
+ * Time: 2:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelpFragment {
+}
