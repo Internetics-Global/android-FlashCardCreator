8608d3be13560e4776047a098ee39e3813cbd9c7
Liang Wang <wangbourne@gmail.com>
2013-06-08 21:53:25 +1000

    Summary: add title of about activity

--- src/com/internectics/android_flashcardcreator/AboutActivity.java
+++ src/com/internectics/android_flashcardcreator/AboutActivity.java
@@ -2,10 +2,13 @@ package com.internectics.android_flashcardcreator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class AboutActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setTitle(R.string.more_about);
         setContentView(R.layout.about);
     }
 
