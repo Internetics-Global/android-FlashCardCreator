a88a3d1094e055d0f2b9a7fd90d3b8c4b769d083
Liang Wang <wangbourne@gmail.com>
2013-11-22 06:28:40 +1000

    update example pack

--- src/com/internectics/android_flashcardcreator/MainActivity.java
+++ src/com/internectics/android_flashcardcreator/MainActivity.java
@@ -315,7 +315,7 @@ public class MainActivity extends FragmentActivity implements
 
         if ((!isDownloaded) && (isReachable) && (mIsFromRestartApp)) {
             mIsFromRestartApp = false;
-            String downloableShareLink = "https://dl.dropbox.com/s/m5dn072h9lfx2ae/new%20example%202013.11.20.zip";
+            String downloableShareLink = "https://dl.dropbox.com/s/noldsfelh2fu0ur/new%20example%202013.11.22.zip";
             File downloadedZipFile = new File(FileOperationHelper.downloadedPackDirectory(), "downloadedPackZip.zip");
             PackDownloadHelper packDownloadHelper = new PackDownloadHelper(MainActivity.this, downloableShareLink, downloadedZipFile.toString());
             packDownloadHelper.mIsFromExamplePackDownload = true;
