9f3840b011810fd5596c118f86f2557ab28270bd
Liang Wang <wangbourne@gmail.com>
2014-01-08 10:42:07 +1000

    fix a bug in android 4.4
    see:
    https://www.dropbox.com/s/2xpudaq2ldmk054/Screenshot_2014-01-08-10-39-16
    .png

--- src/com/internectics/android_flashcardcreator/MainActivity.java
+++ src/com/internectics/android_flashcardcreator/MainActivity.java
@@ -769,7 +769,7 @@ public class MainActivity extends FragmentActivity implements
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT, actionbarHeight,
                 WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH|WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
                 PixelFormat.TRANSLUCENT);
         params.gravity = Gravity.LEFT | Gravity.TOP;
         WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
