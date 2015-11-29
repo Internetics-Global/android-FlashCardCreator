5e2a3cfa21409cf6389068d476ddda044143c26a
Liang Wang <wangbourne@gmail.com>
2015-11-29 16:09:11 +0800

    Support client's user name with space (Flip flash card"）

--- androidFFC/src/main/java/com/flipflash/helper/AWS/AWSUploadHelper.java
+++ androidFFC/src/main/java/com/flipflash/helper/AWS/AWSUploadHelper.java
@@ -115,6 +115,8 @@ public class AWSUploadHelper {
 
             String expectedBucketName = ParseUser.getCurrentUser().getUsername().toLowerCase(); //bucket name必须是low case的，这是aws要求的
 
+            expectedBucketName = expectedBucketName.replace(" ","");
+
             //AWS对于bucket是有命名要求的：http://docs.rightscale.com/faq/clouds/aws/What_are_valid_S3_bucket_names.html
             expectedBucketName = String.format("%s-%s",expectedBucketName,Global.BucketPostfixAfterUserName);
 
