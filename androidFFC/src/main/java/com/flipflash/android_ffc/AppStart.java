a28f8d091b680e239de8e15d184606b58b60d6fb
Liang Wang <wangbourne@gmail.com>
2015-12-10 11:53:26 +0800

    add debug info for better problem tracing

--- androidFFC/src/main/java/com/flipflash/android_ffc/AppStart.java
+++ androidFFC/src/main/java/com/flipflash/android_ffc/AppStart.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
 import com.flipflash.util.AppContext;
+
+import java.util.Random;
+
+import static com.flipflash.util.LogUtils.LOGD;
+import static com.flipflash.util.LogUtils.LOGE;
+
 
 public class AppStart extends Activity {
     private static final String TAG = AppStart.class.getName();
@@ -18,8 +25,9 @@ public class AppStart extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.start);
+
+        collectDeviceInfoForDebugging();
 
         mHandler.sendEmptyMessageDelayed(0,400);
     }
@@ -33,5 +41,14 @@ public class AppStart extends Activity {
             finish();
         }
     };
+
+    private void collectDeviceInfoForDebugging() {
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+        LOGD(TAG, "collectDeviceInfoForDebugging: device width = " + dpWidth + " height = " + dpHeight);
+    }
 
 }
