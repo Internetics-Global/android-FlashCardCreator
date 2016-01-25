55078ca7ce05fd6746b902e8a45dcdcfa5cc04ab
Liang Wang <wangbourne@gmail.com>
2016-01-25 12:31:24 +1000

    change sample pack url

--- androidFFC/src/main/java/com/flipflash/util/Global.java
+++ androidFFC/src/main/java/com/flipflash/util/Global.java
@@ -18,7 +18,7 @@ public class Global {
 
     public static final String BucketPostfixAfterUserName = "55b5aa55673793805862";
 
-    public static final String    SAMPLE_URL = "https://s3.amazonaws.com/internetics.flashcardcreator/Pack1440729625-2043618070.zip?from=Flipflashcards&type=demo";
+    public static final String    SAMPLE_URL = "https://s3-us-west-1.amazonaws.com/ffcmaster/FlipFlashCards.zip?from=Flipflashcards&type=demo";
     //public static final String    SAMPLE_URL = "http://7o51o0.com1.z0.glb.clouddn.com/Pack1440729625-2043618070.zip";
 
     public static final String  DATABASE_NAME = "FlashCardCreator-Local.db";
