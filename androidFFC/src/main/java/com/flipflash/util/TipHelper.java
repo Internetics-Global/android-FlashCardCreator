6e72168a5d9aa271fd9cfc9a7424ae8b83aa06d9
liang wang <wangbourne@gmail.com>
2016-03-17 11:04:46 +1000

    update

--- androidFFC/src/main/java/com/flipflash/util/TipHelper.java
+++ androidFFC/src/main/java/com/flipflash/util/TipHelper.java
@@ -314,7 +314,7 @@ public class TipHelper {
                 .text(activity.getString(R.string.Tip_Toggle))
                 .withArrowLenghtWeight(arrowLength)
                 .toggleArrow(true)
-                .maxWidth(330)
+                .maxWidth(370)
                 .showDelay(100)
                 .activateDelay(150)
                 .withCallback(new TooltipManager.onTooltipClosingCallback() {
